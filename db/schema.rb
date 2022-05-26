cb44eb5e7a32ac272deb8914507281eaa14a6eab
GiancaGrizzly <carloscorteza5@gmail.com>
2022-05-26 19:01:36 +0200

    nuevo scaffold para modelo, color, tapiceria y extra -- gestion de imagenes para cada uno añadida

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_26_155608) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_162157) do
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "colors", force: :cascade do |t|
     t.string "nombre"
     t.float "precio"
-    t.string "pathImagen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -22,7 +49,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_26_155608) do
   create_table "extras", force: :cascade do |t|
     t.string "nombre"
     t.float "precio"
-    t.string "pathImagen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,7 +56,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_26_155608) do
   create_table "modelos", force: :cascade do |t|
     t.string "nombre"
     t.float "precio"
-    t.string "pathImagen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,9 +63,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_26_155608) do
   create_table "tapiceria", force: :cascade do |t|
     t.string "nombre"
     t.float "precio"
-    t.string "pathImagen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
